e20fd621e1871319774386ef54f5e5b410f34da9
cloud-walker <baro.luc@gmail.com>
2016-01-27 17:26:47 +0100

    Add normalize and some basic reset rules

--- tasks/css.js
+++ tasks/css.js
@@ -1,4 +1,6 @@
 import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
 import stylus from 'gulp-stylus'
 import watch from 'gulp-watch'
 import sync from 'browser-sync'
@@ -6,7 +8,16 @@ import config from '../config'
 
 gulp.task('css', function () {
 	gulp.src(`${config.src}/index.styl`)
-		.pipe(stylus())
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'CSS Error',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(stylus({
+			'include css': true,
+			include: ['node_modules']
+		}))
 		.pipe(gulp.dest(config.dest))
 		.pipe(sync.stream())
 })
