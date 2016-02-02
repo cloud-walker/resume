d1e576235fdef48a835fefb83b359aa8ef453e06
cloud-walker <baro.luc@gmail.com>
2016-02-02 06:44:20 +0100

    Add error handling for jade; Add locals to access theme variables to jade

--- tasks/html.js
+++ tasks/html.js
@@ -1,11 +1,30 @@
 import gulp from 'gulp'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
+import data from 'gulp-data'
 import jade from 'gulp-jade'
 import watch from 'gulp-watch'
 import sync from 'browser-sync'
+
 import config from '../config'
+
+import color from '../source/theme/color.json'
+import font from '../source/theme/font.json'
 
 gulp.task('html', function () {
 	gulp.src(`${config.src}/views/**/index.jade`)
+		.pipe(plumber({
+			errorHandler: notify.onError({
+				title: 'HTML Error',
+				message: '<%= error.message %>'
+			})
+		}))
+		.pipe(data(function (file) {
+			return {
+				color: color,
+				font: font
+			}
+		}))
 		.pipe(jade())
 		.pipe(gulp.dest(config.dest))
 		.pipe(sync.stream())
